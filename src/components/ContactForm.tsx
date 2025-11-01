d7ca6dde651cd03cb8e82bca01e38843bcea2cf6
mrrammorkhandikar <iammrrammorkhandikar@gmail.com>
2025-11-01 16:26:29 +0530

    Update ContactForm.tsx

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -75,7 +75,7 @@ const ContactForm = () => {
                             <ContactItem 
                                 icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.8 5.2a2 2 0 002.4 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg>}
                                 title="Email Us"
-                                content={<a href="mailto:contact@ghhclinic.com" className="hover:text-goldAccent">contact@ghhclinic.com</a>}
+                                content={<a href="mailto:contact@ghhclinic.com" className="hover:text-goldAccent">drsayyedbushra@gmail.com</a>}
                             />
                         </div>
 
